dc4bf0b17cb8b4dc0f53ba8888f461b8df57c37d
Philipp Paulweber <p.paulweber@gmail.com>
2015-04-26 02:02:13 +0200

    Repository
    * updated license information

--- src/Type.h
+++ src/Type.h
@@ -1,11 +1,36 @@
-
-/*
-  Copyright (C) 2015 Philipp Paulweber
-  
-  This file is part of the 'libcasm-ir' project which is released under a NSCA
-  open source software license. For more information, see the LICENSE.txt
-  file in the project root directory.
-*/
+//  
+//  Copyright (c) 2015 Philipp Paulweber
+//  All rights reserved.
+//  
+//  Developed by: Philipp Paulweber
+//                https://github.com/ppaulweber/libcasm-ir
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a 
+//  copy of this software and associated documentation files (the "Software"), 
+//  to deal with the Software without restriction, including without limitation 
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//  and/or sell copies of the Software, and to permit persons to whom the 
+//  Software is furnished to do so, subject to the following conditions:
+//  
+//  * Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimers.
+//  
+//  * Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimers in the 
+//    documentation and/or other materials provided with the distribution.
+//  
+//  * Neither the names of the copyright holders, nor the names of its 
+//    contributors may be used to endorse or promote products derived from 
+//    this Software without specific prior written permission.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//  WITH THE SOFTWARE.
+//  
 
 #ifndef _LIB_CASMIR_TYPE_H_
 #define _LIB_CASMIR_TYPE_H_
@@ -48,12 +73,12 @@ namespace libcasm_ir
 #endif /* _LIB_CASMIR_TYPE_H_ */
 
 
-/*
-  Local variables:
-  mode: c++
-  indent-tabs-mode: t
-  c-basic-offset: 4
-  tab-width: 4
-  End:
-  vim:noexpandtab:sw=4:ts=4:
-*/
+//  
+//  Local variables:
+//  mode: c++
+//  indent-tabs-mode: t
+//  c-basic-offset: 4
+//  tab-width: 4
+//  End:
+//  vim:noexpandtab:sw=4:ts=4:
+//  
