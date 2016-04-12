120130571dee9596e25fbcd5edb050aa418f955a
Philipp Paulweber <p.paulweber@gmail.com>
2016-04-12 09:01:46 +0200

    License
    * final license switch from OSL-3.0 to GPLv3
    * modified all files

--- src/Rule.cpp
+++ src/Rule.cpp
@@ -5,18 +5,18 @@
 //  Developed by: Philipp Paulweber
 //                https://github.com/ppaulweber/libcasm-ir
 //  
-//  This software is licensed under the Open Software License v. 3.0 (OSL-3.0).
-//  You either find the license text below or in the root directory of this 
-//  project in the LICENSE.txt file. If you cannot find the LICENSE.txt file,
-//  please refer to https://opensource.org/licenses/OSL-3.0.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //  
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
-//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-//  CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-//  WITH THE SOFTWARE.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
 //  
 
 #include "Rule.h"
