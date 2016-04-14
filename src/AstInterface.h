8964b1200f2e523bb3ecdd90fe5165a8ef2270f5
Philipp Paulweber <p.paulweber@gmail.com>
2016-04-14 13:12:15 +0200

    Repository
    * fixed LICENSE and README text
    * updated all files

--- src/AstInterface.h
+++ src/AstInterface.h
@@ -1,18 +1,20 @@
 //  
-//  Copyright (c) 2016 Philipp Paulweber
+//  Copyright (c) 2015-2016 CASM Organization
 //  All rights reserved.
 //  
 //  Developed by: Philipp Paulweber
 //                https://github.com/ppaulweber/libcasm-ir
 //  
-//  This program is free software: you can redistribute it and/or modify
+//  This file is part of libcasm-ir.
+//  
+//  libcasm-ir is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //  
-//  This program is distributed in the hope that it will be useful,
+//  libcasm-ir is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
 //  
 //  You should have received a copy of the GNU General Public License
