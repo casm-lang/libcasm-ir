70f44e3ee25e2e56b2d4ec836b42435bcc41f887
Philipp Paulweber <p.paulweber@gmail.com>
2016-11-18 17:53:07 +0100

    License
    * updated license information for all files

--- src/Statement.h
+++ src/Statement.h
@@ -1,25 +1,25 @@
-//  
+//
 //  Copyright (c) 2015-2016 CASM Organization
 //  All rights reserved.
-//  
+//
 //  Developed by: Philipp Paulweber
 //                https://github.com/casm-lang/libcasm-ir
-//  
+//
 //  This file is part of libcasm-ir.
-//  
+//
 //  libcasm-ir is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  libcasm-ir is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with libcasm-ir. If not, see <http://www.gnu.org/licenses/>.
-//  
+//
 
 #ifndef _LIB_CASMIR_STATEMENT_H_
 #define _LIB_CASMIR_STATEMENT_H_
