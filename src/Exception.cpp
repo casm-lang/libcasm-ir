f7639492f603f21a25197333f79a7cafb72c207c
Philipp Paulweber <p.paulweber@gmail.com>
2017-06-06 17:26:23 +0200

    Exceptions
    * added new IR related 'TypeArgumentException', which is used to
      indicate e.g. in the type annotation object to indicate incorrect
      argument relations etc.

---
+++ src/Exception.cpp
@@ -0,0 +1,53 @@
+//
+//  Copyright (c) 2015-2017 CASM Organization
+//  All rights reserved.
+//
+//  Developed by: Philipp Paulweber
+//                https://github.com/casm-lang/libcasm-ir
+//
+//  This file is part of libcasm-ir.
+//
+//  libcasm-ir is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  libcasm-ir is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with libcasm-ir. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "Exception.h"
+
+using namespace libcasm_ir;
+
+TypeArgumentException::TypeArgumentException(
+    const std::string& message, const std::size_t position )
+: m_message( message )
+, m_position( position )
+{
+}
+
+const char* TypeArgumentException::what() const noexcept
+{
+    return m_message.c_str();
+}
+
+std::size_t TypeArgumentException::position( void ) const noexcept
+{
+    return m_position;
+}
+
+//
+//  Local variables:
+//  mode: c++
+//  indent-tabs-mode: nil
+//  c-basic-offset: 4
+//  tab-width: 4
+//  End:
+//  vim:noexpandtab:sw=4:ts=4:
+//
